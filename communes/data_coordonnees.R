407380d8e7ed0ab36daa37850b008dee767b776a
blaquans <pachevalier@gmail.com>
2014-03-05 17:47:29 +0100

    Corrections

--- communes/data_coordonnees.R
+++ communes/data_coordonnees.R
@@ -1,18 +1,18 @@
 # Sources : http://public.opendatasoft.com/explore/dataset/correspondance-code-insee-code-postal/#?tab=export&start=20
-df  <- read.csv2("data/correspondance-code-insee-code-postal.csv", stringsAsFactors = FALSE)
-names(df)
-str(df)
-df$geo_shape  <- NULL
-df$geo_point_2d
-str(df)
-coord  <- data.frame(t(as.data.frame(str_split(df$geo_point_2d, ","))))
+dfm  <- read.csv2("data/correspondance-code-insee-code-postal.csv", stringsAsFactors = FALSE)
+names(dfm)
+str(dfm)
+dfm$geo_shape  <- NULL
+dfm$geo_point_2d
+str(dfm)
+coord  <- data.frame(t(as.data.frame(str_split(dfm$geo_point_2d, ","))))
 str(coord)
 names(coord)  <- c("lat", "lon")
 coord$lat  <- as.numeric(as.character(coord$lat))
 coord$lon  <- as.numeric(as.character(coord$lon))
-df  <- cbind(df, coord)
-str(df)
-dfm  <- subset(df, nom_region %in% c("GUYANE", "GUADELOUPE", "REUNION", "MARTINIQUE", "MAYOTTE") == FALSE) 
+dfm  <- cbind(dfm, coord)
+str(dfm)
+#dfm  <- subset(dfm, nom_region %in% c("GUYANE", "GUADELOUPE", "REUNION", "MARTINIQUE", "MAYOTTE") == FALSE) 
 str(dfm)
 dfm$insee_com[dfm$insee_com == "75101"]  <- "75056"
 dfm$insee_com[dfm$insee_com == "69381"]  <- "69123"
